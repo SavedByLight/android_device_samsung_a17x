15d37906212cd96a97d93eaafaf106d267ada85b
mrfluffyoven <mrfluffyoven@gmail.com>
2025-12-22 08:01:12 +0000

    support other recoveries
    Change-Id: I493f88e577e0b007fba56b8cbcd5fe9dbf91e9d0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,36 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The OrangeFox Recovery Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# For building with minimal manifest
 export ALLOW_MISSING_DEPENDENCIES=true
+
+FDEVICE="s5e8535"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+	if [ -n "$chkdev" ]; then
+		FOX_BUILD_DEVICE="$FDEVICE"
+	else
+		chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+		[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+	fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+	fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+export LC_ALL="C"
+
+	# Let's see which are our build vars
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+        export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+        export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+    fi
+fi
